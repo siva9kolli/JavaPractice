99ad4761c9055275839f713c2153ddfa2c63e1a5
Shiv <siva9kolli@gmail.com>
2021-06-29 11:20:19 +0530

    waits

--- src/test/java/com/MyCompany/SeleniumTests/SeleniumWaits.java
+++ src/test/java/com/MyCompany/SeleniumTests/SeleniumWaits.java
@@ -4,9 +4,13 @@ import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import org.testng.annotations.AfterTest;
@@ -17,16 +21,19 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class SeleniumWaits {
 
-	WebDriver driver;
+	public WebDriver driver;
 
 	@BeforeTest
 	public void startBrowser() {
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
+		//driver.get("https://parabank.parasoft.com/");
+		driver.get("http://automationpractice.com/index.php");
+		driver.manage().window().maximize();
 	}
 
-	@Test
-	public void loginAsAdmin() {
+	//@Test
+	public void implictWaitTest() {
 
 		driver.get("http://automationpractice.com/index.php");
 		driver.manage().window().maximize();
@@ -42,14 +49,39 @@ public class SeleniumWaits {
 	}
 
 	
-	@Test
-	public void expilcitWaits() {
-		driver.get("https://parabank.parasoft.com/");
-		driver.manage().window().maximize();
-		WebDriverWait wait = new WebDriverWait(driver, 30);		
-		wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.className("logi"))));
-		driver.findElement(By.className("login")).click();
+	//@Test
+	public void expilcitWaitTest() {
+		WebDriverWait wait = new WebDriverWait(driver, 20);
+		//wait.until(ExpectedConditions.elementToBeClickable(By.xpath("Test")));
+		wait.until(ExpectedConditions.attributeContains(By.cssSelector("input.button"), "value", "Log In"));
+		wait.until(ExpectedConditions.titleIs("ParaBank | Welcome | Online Banking"));
+		boolean b = wait.until(ExpectedConditions.attributeToBe(By.cssSelector("input.button"), "value", "Log In"));
+		System.out.println("boolean ==" + b);
+		wait.until(
+				ExpectedConditions
+				.elementToBeClickable(By.cssSelector("input.button"))).click();
+		
+		wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("input.button")));
+		driver.findElement(By.cssSelector("input.button")).click();
+		
 	}
+	
+	
+	@Test
+	public void fluentWaitTest() {
+		FluentWait<WebDriver> fWait = new FluentWait<WebDriver>(driver)
+				.withTimeout(Duration.ofSeconds(10))
+				.pollingEvery(Duration.ofSeconds(4))
+				.ignoring(NoSuchElementException.class)
+				.ignoring(TimeoutException.class)
+				.withMessage("Element not found");
+		
+		fWait.until(ExpectedConditions.elementToBeClickable(By.xpath("Path")));
+		
+	}
+	
+	
+	
 	@AfterTest
 	public void quitSession() {
 		driver.quit();
